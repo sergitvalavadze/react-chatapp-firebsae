06980843e8509d68108c7d7acb5cf5cebcdc5129
sergitvalavadze <sergi.tvalavadze@gmail.com>
2020-09-08 08:57:25 +0800

    user info display on sidebar menu

--- src/firebase.js
+++ src/firebase.js
@@ -4,16 +4,16 @@ import "firebase/database";
 import "firebase/storage";
 
 var firebaseConfig = {
-    apiKey: "AIzaSyC1WAlANRx1e9oVNmTk8btgf-6HUMCdpsw",
-    authDomain: "sergi-react-slack.firebaseapp.com",
-    databaseURL: "https://sergi-react-slack.firebaseio.com",
-    projectId: "sergi-react-slack",
-    storageBucket: "sergi-react-slack.appspot.com",
-    messagingSenderId: "910190693407",
-    appId: "1:910190693407:web:40c39da2a867c9ce72192a",
-    measurementId: "G-L07THDSWY4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyC1WAlANRx1e9oVNmTk8btgf-6HUMCdpsw",
+  authDomain: "sergi-react-slack.firebaseapp.com",
+  databaseURL: "https://sergi-react-slack.firebaseio.com",
+  projectId: "sergi-react-slack",
+  storageBucket: "sergi-react-slack.appspot.com",
+  messagingSenderId: "910190693407",
+  appId: "1:910190693407:web:40c39da2a867c9ce72192a",
+  measurementId: "G-L07THDSWY4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 export default firebase;
